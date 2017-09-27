23cd1b1d98d808b9bc333a503a05762e9e12a894
lecas93 <angi_lecas@hotmail.com>
2017-09-27 01:38:02 -0500

    Creada conexion basica a BD, funcionando

--- src/DB.cpp
+++ src/DB.cpp
@@ -6,12 +6,28 @@
  */
 
 //La BD se planea hacerla con sqlite :)
-
+#include <sqlite3.h>
 #include "DB.h"
+
+class sqlite3;
 
 DB::DB() {
 	// TODO Auto-generated constructor stub
+	sqlite3* db;
+	//char* zErrMsg = 0;
+	int rc;
 
+	rc = sqlite3_open("test.db", &db);
+
+	if (rc) {
+		//fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		cout<<"No se pudo abrir la DB"<<endl;
+		//return (0);
+	} else {
+		//fprintf(stderr, "Opened database successfully\n");
+		cout<<"Se pudo abrir la DB"<<endl;
+	}
+	sqlite3_close(db);
 }
 
 DB::~DB() {
